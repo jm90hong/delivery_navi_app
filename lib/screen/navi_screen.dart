d1cc0b8ce78595951fbdeace153bf265719750ce
jm90hong <jm90.hong@gmail.com>
2024-01-09 13:43:56 +0900

    1

---
+++ lib/screen/navi_screen.dart
@@ -0,0 +1,23 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class NaviScreen extends StatefulWidget {
+  const NaviScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NaviScreen> createState() => _NaviScreenState();
+}
+
+class _NaviScreenState extends State<NaviScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+      ),
+    );
+  }
+}
